b654be8d010fe0ef8c6b0c35c1c55a8d51f1aaa4
Afonso Mateus <afonsomateus.dev@gmail.com>
2024-03-29 12:30:08 -0300

    feature: criacao da pagina Home

---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+export function Home() {
+  return (
+    <main className="bg-orange-600 h-screen">
+      <h1 className="font-roboto font-bold text-5xl text-center">Behaviorismo?</h1>
+    </main>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        orange: {
+          600: '#d15404'
+        },
+      },
+      fontFamily: {
+        'roboto': ['Roboto', 'system-ui', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
