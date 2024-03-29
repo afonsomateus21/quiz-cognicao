f7ceaae9af125aea9b4f0efb156eb13e69a43580
Afonso Mateus <afonsomateus.dev@gmail.com>
2024-03-29 12:34:33 -0300

    fix: ajustes na pagina Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
 export function Home() {
   return (
-    <main className="bg-orange-600 h-screen">
-      <h1 className="font-roboto font-bold text-5xl text-center">Behaviorismo?</h1>
+    <main className="bg-orange-600 h-screen flex flex-col justify-center">
+      <div className="my-0 mx-auto h-3/4 w-3/4">
+        <h1 className="font-roboto font-bold text-5xl text-center">Behaviorismo?</h1>
+      </div>
     </main>
   );
 }
