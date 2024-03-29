c001f21f9c51d55b116a520799fba7bd34543b04
Afonso Mateus <afonsomateus.dev@gmail.com>
2024-03-29 13:25:50 -0300

    fix: ajuste na cor do texto do ActionButton

--- src/components/ActionButton.tsx
+++ src/components/ActionButton.tsx
@@ -14,7 +14,7 @@ export function ActionButton({ title, icon, ...rest }: ActionButtonProps) {
       <div className="bg-lime-900 rounded-full size-8 flex flex-col justify-center items-center mr-2">
         <span>{ icon }</span>
       </div>
-      <span>{ title }</span>
+      <span className="text-lime-900">{ title }</span>
     </button>
   );
 }
