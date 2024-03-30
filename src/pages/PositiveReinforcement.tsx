1eeae15cf6838230b7380c2a763ef9a8a9d98e9d
Afonso Mateus <afonsomateus.dev@gmail.com>
2024-03-30 13:13:22 -0300

    feature: adicao dos componentes de navegacao a pagina PositiveReinforcement

--- src/pages/PositiveReinforcement.tsx
+++ src/pages/PositiveReinforcement.tsx
@@ -1,14 +1,31 @@
+import { ArrowLeft, ArrowRight } from "lucide-react";
+import { NavArrowButton } from "../components/NavArrowButton";
+
 export function PositiveReinforcement() {
   return (
     <main className="bg-lime-900 h-screen flex flex-col justify-center overflow-hidden">
       <div className=" p-2 md:p-0 my-0 mx-auto h-full w-full md:h-3/4 md:w-3/4 flex flex-col gap-10 justify-between items-center">
-        <div>
+        <div className="w-full flex justify-between items-center">
+          <NavArrowButton 
+            to="/reinforcements"
+            children={
+              <ArrowLeft color="#394f21" size={60} />
+            } 
+            color="bg-yellow-600" 
+          />
           <h1 className="text-5xl font-roboto font-bold text-white">Reforço Positivo</h1>
+          <NavArrowButton 
+            to="/reinforcements/negative-reinforcement"
+            children={
+              <ArrowRight color="#394f21" size={60} />
+            } 
+            color="bg-orange-600" 
+          />
         </div>
 
         <section className="h-4/5 flex flex-col justify-center  bg-yellow-600 rounded-3xl p-8">
           <div className="p-4 overflow-scroll overflow-y-scroll h-4/5 overflow-x-hidden">
-            <p className="text-justify text-md text-white">
+            <p className="text-justify text-md text-white font-roboto">
               Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
               Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
               when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
