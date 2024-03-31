be109ca186919d74aa42676947c13316806cd885
Afonso Mateus <afonsomateus.dev@gmail.com>
2024-03-30 22:05:36 -0300

    feature: criacao da constante questions que guarda todas as perguntas do quiz

---
+++ src/constants/questions.ts
@@ -0,0 +1,184 @@
+[
+  {
+    "questionId": "c84106b3-41ec-4c29-9c7d-f7c1a3e13a0c",
+    "question": "Comportamento voluntário, que opera a partir de estímulos.' e 'Uma resposta não é causada por um único estímulo' são explicações importantes para determinar o conceito de:",
+    "answers": [
+      { 
+        "answerId": "c544b921-d4a2-4a7c-af0b-226fdd208c29", 
+        "text": "Condicionamento clássico",
+        "isCorrect": true 
+      },
+      { 
+        "answerId": "81fb8f40-227f-4b6c-a2e7-560ab9287699", 
+        "text": "Condicionamento operante",
+        "isCorrect": false 
+      },
+      { 
+        "answerId": "7922da0d-30a5-45b5-8fa7-6071d5456b14", 
+        "text": "Neuro condicionamento",
+        "isCorrect": false 
+      },
+      { 
+        "answerId": "8d2e81fb-4b2d-43df-a5e0-fdf6e08a8a0c", 
+        "text": "Condicionamento divergente",
+        "isCorrect": false 
+      }
+    ]
+  },
+  {
+    "questionId": "7fc7bcfd-d72b-4c65-b6b7-8a8b0a0d2e64",
+    "question": "Qual o nome do tipo de consequência ao comportamento que diminui a probabilidade de um determinado comportamento voltar a acontecer.",
+    "answers": [
+      { 
+        "answerId": "55e3d6fc-4f8d-4141-bd95-03f6a7c5e860", 
+        "text": "Reforço",
+        "isCorrect": false 
+      },
+      { 
+        "answerId": "76b40e5a-84f2-48da-a4c1-670f3a5220a0", 
+        "text": "Auxílio",
+        "isCorrect": false 
+      },
+      { 
+        "answerId": "d349c999-f9e5-45ef-a389-1aaf9f3a6e57", 
+        "text": "Punição",
+        "isCorrect": true 
+      },
+      { 
+        "answerId": "edba4a41-7696-4580-b2a8-71b5e792bb0c", 
+        "text": "Correção",
+        "isCorrect": false 
+      }
+    ]
+  },
+  {
+    "questionId": "2af89141-8653-4b54-8926-47b8a7b146e3",
+    "question": "Qual o tipo de consequência do comportamento que aumenta a probabilidade de um determinado comportamento voltar a acontecer.",
+    "answers": [
+      { 
+        "answerId": "a4ac11b5-17e0-4e5b-afaf-1e4c0cb5ef3a", 
+        "text": "Reforço",
+        "isCorrect": true 
+      },
+      { 
+        "answerId": "63b54c7a-d5b5-4a4c-b1e0-50d0bdc32f37", 
+        "text": "Auxílio",
+        "isCorrect": false 
+      },
+      { 
+        "answerId": "279f6aaf-c342-4b2f-8b78-408db2259821", 
+        "text": "Punição",
+        "isCorrect": false 
+      },
+      { 
+        "answerId": "d4d0d6f4-2b16-4d35-9b2b-50d185eb05a8", 
+        "text": "Correção",
+        "isCorrect": false 
+      }
+    ]
+  },
+  {
+    "questionId": "f3c3b3bf-81f2-4924-94ac-c8a72b5a06b7",
+    "question": "Um elemento aversivo ao indivíduo é retirado como um reforço para a continuação de um comportamento. Chamamos essa ação de:",
+    "answers": [
+      { 
+        "answerId": "e9a78949-7bcb-43ac-b95c-2fda8be36149", 
+        "text": "Reforço positivo",
+        "isCorrect": false 
+      },
+      { 
+        "answerId": "bdf4a11c-52d5-4748-aa2a-79dc5e6f93b0", 
+        "text": "Reforço negativo",
+        "isCorrect": true 
+      },
+      { 
+        "answerId": "a8a0c00b-bc0b-43bc-ae1d-9e49494f12e7", 
+        "text": "Punição negativa",
+        "isCorrect": false 
+      },
+      { 
+        "answerId": "6f201417-0f24-4373-bd80-525d2335db8d", 
+        "text": "Punição positiva",
+        "isCorrect": false 
+      }
+    ]
+  },
+  {
+    "questionId": "6372bfc1-14cb-47a7-98f2-24b8bdf1755e",
+    "question": "Como chamamos quando um elemento aversivo é apresentado após a realização de um comportamento não desejado?",
+    "answers": [
+      { 
+        "answerId": "2eb2cd47-227d-47b8-9ba7-eb5ac6e122a5", 
+        "text": "Reforço positivo",
+        "isCorrect": false 
+      },
+      { 
+        "answerId": "2d8ff0e9-8dd5-42a6-9eaa-dba26de9b356", 
+        "text": "Reforço negativo",
+        "isCorrect": false 
+      },
+      { 
+        "answerId": "137a890b-134a-4c78-bc6d-2678264b9d63", 
+        "text": "Punição negativa",
+        "isCorrect": false 
+      },
+      { 
+        "answerId": "5820a2df-5560-48e8-bb02-9d8f890992b7", 
+        "text": "Punição positiva",
+        "isCorrect": true 
+      }
+    ]
+  },
+  {
+    "questionId": "a9f8b497-8a02-416f-a5fb-73bcf65c10f3",
+    "question": "Qual o conceito caracterizado pela retirada de um elemento gratificante ao indivíduo como uma punição para a diminuição de um comportamento.",
+    "answers": [
+      { 
+        "answerId": "b83b55e3-f96a-42f1-9c6a-ee2394a172cb", 
+        "text": "Reforço positivo",
+        "isCorrect": false 
+      },
+      { 
+        "answerId": "e1bba49b-d0a7-4768-86b1-6f57b41ed52f", 
+        "text": "Reforço negativo",
+        "isCorrect": false 
+      },
+      { 
+        "answerId": "edf1f47d-31d7-4181-b69e-1d9b12824259", 
+        "text": "Punição negativa",
+        "isCorrect": true 
+      },
+      { 
+        "answerId": "e6fe4401-4b10-4841-9f99-1461ad961750", 
+        "text": "Punição positiva",
+        "isCorrect": false 
+      }
+    ]
+  },
+  {
+    "questionId": "9a65de0d-c55b-4654-9868-196d5ab7fc9b",
+    "question": "Marque a alternativa que completa a lacuna: No _________________ um elemento de recompensa é adicionado quando o comportamento desejado é alcançado.",
+    "answers": [
+      { 
+        "answerId": "4d0374a2-7be4-4a97-863b-b62d6c688304", 
+        "text": "Reforço positivo",
+        "isCorrect": true 
+      },
+      { 
+        "answerId": "45d51e88-d855-4aa3-b9ed-eb3f8c8f49df", 
+        "text": "Reforço negativo",
+        "isCorrect": false 
+      },
+      { 
+        "answerId": "7006e318-1244-499e-91c2-84a055da83d1", 
+        "text": "Punição negativa",
+        "isCorrect": false 
+      },
+      { 
+        "answerId": "bc1e5976-1c08-4e1d-90b2-bb31546e4010", 
+        "text": "Punição positiva",
+        "isCorrect": false 
+      }
+    ]
+  }
+]
