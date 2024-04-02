5d62b0149ce0ace04db645064e36b6aab177e891
Afonso Mateus <afonsomateus.dev@gmail.com>
2024-04-02 20:21:07 -0300

    fix: ajustes na responsividade da pagina About

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,17 +2,17 @@ import SeriousBrain from "../assets/serious-brain.png";
 
 export function About() {
   return (
-    <main className="bg-yellow-600 h-screen flex flex-col justify-center relative overflow-hidden">
-      <div className=" p-2 md:p-6 lg:p-0 my-0 ml-auto h-full w-full md:h-[90%] lg:w-[90%] flex flex-col justify-center gap-6">
+    <main className="bg-yellow-600 min-h-screen flex flex-col justify-center relative overflow-hidden">
+      <div className="p-2 md:p-6 lg:p-0 my-0 ml-auto h-full w-full md:h-[90%] lg:w-[90%] flex flex-col justify-center gap-6 overflow-y-auto">
         <div className="w-full">
-          <h1 className="text-white font-bold text-6xl text-left">
+          <h1 className="text-white font-bold text-2xl md:text-6xl text-left">
             Olá, <span className="text-lime-900">cognauta</span>!
           </h1>
         </div>
 
-        <div className="flex justify-between w-full">
-          <div className="w-3/4">
-            <p className="text-white text-xl text-justify font-bold">
+        <div className="flex flex-col md:flex-row justify-between">
+          <div className="w-full md:w-3/4 md:pr-6 h-[500px] overflow-scroll overflow-x-hidden">
+            <p className="text-white text-sm md:text-xl text-justify font-bold">
               Contrary to popular belief, Lorem Ipsum is not simply random text. 
               It has roots in a piece of classical Latin literature from 45 BC, 
               making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, 
@@ -27,7 +27,7 @@ export function About() {
               exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
             </p>
 
-            <p className="text-white text-xl text-justify mt-5 font-bold">
+            <p className="text-white text-sm md:text-xl text-justify font-bold">
               Contrary to popular belief, Lorem Ipsum is not simply random text. 
               It has roots in a piece of classical Latin literature from 45 BC, 
               making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, 
@@ -43,17 +43,17 @@ export function About() {
             </p>
           </div>
 
-          <div className=" flex-1">
-            <div className="bg-lime-900 rounded-3xl h-full w-[400px] ml-auto flex flex-col justify-center items-center">
-              <img className="h-96 w-72 flex justify-center items-center" src={ SeriousBrain } alt="Ilustração de um cérebro com rosto sério" />
+          <div className="w-full md:w-1/4 flex justify-center mt-6 md:mt-0">
+            <div className="bg-lime-900 rounded-3xl h-full w-[300px] md:w-[400px] lg:w-[500px] xl:w-[600px] ml-auto flex justify-center items-center">
+              <img className="h-72 w-56 lg:h-96 md:w-72" src={SeriousBrain} alt="Ilustração de um cérebro com rosto sério" />
             </div>
           </div>
         </div>
       </div>
 
-      <div className="h-[600px] w-24 bg-orange-600 rounded-r-[30px] absolute" />
+      <div className="h-[600px] hidden md:block lg:w-12 bg-orange-600 rounded-r-[30px] absolute" />
 
-      <div className="size-[400px] bg-orange-600 rounded-full absolute right-[-150px] bottom-[-150px]"/>
+      <div className="size-[300px] md:size-[400px] bg-orange-600 rounded-full absolute right-[-170px] bottom-[-80px]" />
     </main>
   );
 }
