3cab2b463100c36dbf60c9d5234141ba9dea9332
Afonso Mateus <afonsomateus.dev@gmail.com>
2024-04-03 00:00:29 -0300

    fix: removendo router do main

--- src/main.tsx
+++ src/main.tsx
@@ -1,69 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Home } from './pages/Home.tsx';
-import { Question } from './pages/Question.tsx';
-import { DecisionContinueOrStudy } from './pages/DecisionContinueOrStudy.tsx';
-import { ReinforcementsDescription } from './pages/ReinforcementsDescription.tsx';
-import { PositiveReinforcement } from './pages/PositiveReinforcement.tsx';
-import { NegativeReinforcement } from './pages/NegativeReinforcement.tsx';
-import { GameOver } from './pages/GameOver.tsx';
 import { PlayerProvider } from './hooks/usePlayer.tsx';
-import { GameWinner } from './pages/GameWinner.tsx';
-import { About } from './pages/About.tsx';
-import { CreditsAndReference } from './pages/CreditsAndReference.tsx';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes.tsx';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <Home />
-      },
-      {
-        path: "/questions",
-        element: <Question />
-      },
-      {
-        path: "/game-over",
-        element: <GameOver />
-      }
-    ]
-  }, 
-  {
-    path: "/decision-continue-or-study",
-    element: <DecisionContinueOrStudy />
-  },
-  {
-    path: "/reinforcements",
-    element: <ReinforcementsDescription />
-  },
-  {
-    path: "/reinforcements/positive-reinforcement",
-    element: <PositiveReinforcement />
-  },
-  {
-    path: "/reinforcements/negative-reinforcement",
-    element: <NegativeReinforcement />
-  },
-  {
-    path: "/game-winner",
-    element: <GameWinner />
-  },
-  {
-    path: "/about",
-    element: <About />
-  },
-  {
-    path: "/credits",
-    element: <CreditsAndReference />
-  }
-])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
---
+++ src/routes.tsx
@@ -0,0 +1,62 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+import App from './App.tsx'
+import { Home } from './pages/Home.tsx';
+import { Question } from './pages/Question.tsx';
+import { DecisionContinueOrStudy } from './pages/DecisionContinueOrStudy.tsx';
+import { ReinforcementsDescription } from './pages/ReinforcementsDescription.tsx';
+import { PositiveReinforcement } from './pages/PositiveReinforcement.tsx';
+import { NegativeReinforcement } from './pages/NegativeReinforcement.tsx';
+import { GameOver } from './pages/GameOver.tsx';
+import { GameWinner } from './pages/GameWinner.tsx';
+import { About } from './pages/About.tsx';
+import { CreditsAndReference } from './pages/CreditsAndReference.tsx';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/questions",
+        element: <Question />
+      },
+      {
+        path: "/game-over",
+        element: <GameOver />
+      }
+    ]
+  }, 
+  {
+    path: "/decision-continue-or-study",
+    element: <DecisionContinueOrStudy />
+  },
+  {
+    path: "/reinforcements",
+    element: <ReinforcementsDescription />
+  },
+  {
+    path: "/reinforcements/positive-reinforcement",
+    element: <PositiveReinforcement />
+  },
+  {
+    path: "/reinforcements/negative-reinforcement",
+    element: <NegativeReinforcement />
+  },
+  {
+    path: "/game-winner",
+    element: <GameWinner />
+  },
+  {
+    path: "/about",
+    element: <About />
+  },
+  {
+    path: "/credits",
+    element: <CreditsAndReference />
+  }
+])
