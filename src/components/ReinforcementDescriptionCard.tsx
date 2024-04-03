bf06f4ca19c6b9bb030f8728c633b00e712d8c5f
Afonso Mateus <afonsomateus.dev@gmail.com>
2024-04-02 23:44:41 -0300

    fix: ajustes na responsividade do componente ReinforcementDescriptionCard

--- src/components/ReinforcementDescriptionCard.tsx
+++ src/components/ReinforcementDescriptionCard.tsx
@@ -11,7 +11,9 @@ export function ReinforcementDescriptionCard({ color, title, text, children } : 
   return (
     <section className={`${color} w-[90%] md:w-full h-96 md:h-64 lg:h-56 p-4 rounded-3xl relative`}>
       <h1 className="text-white text-3xl font-bold">{ title }</h1>
-      <p className="text-white text-justify">{ text }</p>
+      <div className="overflow-scroll md:overflow-hidden overflow-x-hidden h-3/4 md:h-auto">
+        <p className="text-white text-justify">{ text }</p>
+      </div>
       <div className="absolute right-2 bottom-2">
         { children }
       </div>
